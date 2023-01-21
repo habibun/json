8194e0dc690cb88516d9809e3a85a87f11c4b354
Habibun Noby <habibun.noby@gmail.com>
2023-01-21 09:34:51 +0600

    refactor: rearrange folders

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # json
 JavaScript Object Notation
+
+### License
+The code is licensed under the [MIT License][license]
+
+
+[license]: https://github.com/habibun/json/blob/main/LICENSE
---
+++ Tutorial/W3schools/assets/customers_mysql.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jony
+ * Date: 8/26/16
+ * Time: 3:32 PM
+ */
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$conn = new mysqli("myServer", "myUser", "myPassword", "Northwind");
+
+$result = $conn->query("SELECT CompanyName, City, Country FROM Customers");
+
+$outp = "[";
+while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+    if ($outp != "[") {$outp .= ",";}
+    $outp .= '{"Name":"'  . $rs["CompanyName"] . '",';
+    $outp .= '"City":"'   . $rs["City"]        . '",';
+    $outp .= '"Country":"'. $rs["Country"]     . '"}';
+}
+$outp .="]";
+
+$conn->close();
+
+echo($outp);
+?>
